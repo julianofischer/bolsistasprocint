e1e3380caa3b8552e58a3ff0666274252f4785bb
Juliano Naves <juliano.naves@geospan.com>
2023-10-02 17:02:00 -0400

    ...

--- reports/views.py
+++ reports/views.py
@@ -1,4 +1,5 @@
 from typing import Any, Dict
+from django.db import models
 from django.db.models.query import QuerySet
 from django.shortcuts import render
 
@@ -9,8 +10,9 @@ from .forms import LoginForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from .models import Report, ReportEntry
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 def login_view(request):
@@ -66,15 +68,18 @@ class ReportEntriesListView(LoginRequiredMixin, ListView):
     
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         data = super().get_context_data(**kwargs)
-        data["last_entry"] = ReportEntry.objects.filter(report__id=self.kwargs['report_id']).last()
+        data["display_entry"] = ReportEntry.objects.filter(report__id=self.kwargs['report_id']).last()
         data["report_id"] = self.kwargs['report_id']
+        data["edit_mode"] = self.kwargs.get('edit_mode', False)
+        data["total_hours"] = Report.objects.get(id=self.kwargs['report_id']).total_hours
         return data
     
 
-class ReportEntryCreateView(LoginRequiredMixin, CreateView):
+class ReportEntryCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     model = ReportEntry
     fields = ['description', 'date', 'init_hour', 'end_hour']
     template_name = 'reports/report_entries.html'
+    success_message = "Entrada criada com sucesso!"
 
     def form_valid(self, form):
         form.instance.report_id = self.kwargs['report_id']
@@ -85,11 +90,38 @@ class ReportEntryCreateView(LoginRequiredMixin, CreateView):
         return reverse_lazy('report-entries', kwargs={'report_id': self.kwargs['report_id']})
     
 
-class ReportEntryUpdateView(LoginRequiredMixin, UpdateView):
+# PARECIDA COM A DE CIMA, EDITANDO UMA ENTRY
+class ReportEntryUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = ReportEntry
     fields = ['description', 'date', 'init_hour', 'end_hour']
-    template_name = 'reports/create_update_report_entry.html'
+    template_name = 'reports/report_entries.html'
+    context_object_name = 'entry'
+    success_message = "Entrada editada com sucesso!"
 
-    
+    def get_queryset(self) -> QuerySet[Any]:
+        report_id = self.kwargs['report_id']
+        # Query all entries related to the report
+        return ReportEntry.objects.filter(report__id=report_id)
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        data = super().get_context_data(**kwargs)
+        data["display_entry"] = ReportEntry.objects.get(id=self.kwargs["pk"])
+        data["report_id"] = self.kwargs['report_id']
+        data["entry_id"] = self.kwargs["pk"]
+        data["edit_mode"] = True
+        data["entries"] = ReportEntry.objects.filter(report__id=self.kwargs['report_id'])
+        data["total_hours"] = Report.objects.get(id=self.kwargs['report_id']).total_hours
+        return data
+
+    def get_success_url(self):
+        # Define the URL where you want to redirect after a successful form submission
+        return reverse_lazy('report-entries', kwargs={'report_id': self.kwargs['report_id']})
 
 
+class ReportEntryDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
+    model = ReportEntry
+    success_message = "Entrada deletada com sucesso!"
+
+    def get_success_url(self):
+        # Define the URL where you want to redirect after a successful form submission
+        return reverse_lazy('report-entries', kwargs={'report_id': self.kwargs['report_id']})
